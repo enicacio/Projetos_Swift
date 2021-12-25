0d4a29993ec7b42d50e699347fb35e931acd05cc
Eliane Regina Nicácio Mendes <elianemendes@Elianes-MacBook-Air.local>
2021-12-25 12:38:46 -0300

    feat: primeiro hello world

---
+++ Hello, world!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello, world!
+//
+//  Created by Eliane Regina Nicácio Mendes on 25/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Hello, world!UITests/Hello__world_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Hello__world_UITestsLaunchTests.swift
+//  Hello, world!UITests
+//
+//  Created by Eliane Regina Nicácio Mendes on 25/12/21.
+//
+
+import XCTest
+
+class Hello__world_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
